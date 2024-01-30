efef26d9f0d5dc9e81f4fed3ac689bccfd95cf3a
Ben Awad <benawad97@gmail.com>
2024-01-30 15:45:57 -0600

    height and width static

--- web/src/app/chart/metric-selector/MetricSelector.tsx
+++ web/src/app/chart/metric-selector/MetricSelector.tsx
@@ -17,60 +17,64 @@ export const MetricSelector: React.FC<MetricSelectorProps> = ({
   const { data } = trpc.getEventNames.useQuery({ projectId });
 
   return (
-    <div style={{ width: 320 }}>
-      <Downshift
-        onChange={(selection) =>
-          onEventNameChange(selection ? selection.value : "")
-        }
-        itemToString={(item) => (item ? item.value : "")}
-        initialIsOpen
-      >
-        {({
-          getInputProps,
-          getItemProps,
-          getLabelProps,
-          getMenuProps,
-          isOpen,
-          inputValue,
-          highlightedIndex,
-          selectedItem,
-          getRootProps,
-        }) => (
-          <div className="bg-primary-700">
-            {/* <label {...getLabelProps()}>Enter a fruit</label> */}
-            <div
-              style={{ display: "inline-block" }}
-              className="w-full"
-              {...getRootProps({}, { suppressRefError: true })}
-            >
-              <Input {...getInputProps({ autoFocus: true })} />
-            </div>
-            <ul {...getMenuProps()}>
-              {isOpen
-                ? data?.names
-                    .filter((item) => !inputValue || item.includes(inputValue))
-                    .map((item, index) => (
-                      <li
-                        {...getItemProps({
-                          key: item,
-                          index,
-                          item: { value: item },
-                          style: {
-                            backgroundColor:
-                              highlightedIndex === index ? "lightgray" : "#000",
-                            fontWeight:
-                              selectedItem === item ? "bold" : "normal",
-                          },
-                        })}
-                      >
-                        {item}
-                      </li>
-                    ))
-                : null}
-            </ul>
+    <Downshift
+      onChange={(selection) =>
+        onEventNameChange(selection ? selection.value : "")
+      }
+      itemToString={(item) => (item ? item.value : "")}
+      initialIsOpen
+    >
+      {({
+        getInputProps,
+        getItemProps,
+        getLabelProps,
+        getMenuProps,
+        isOpen,
+        inputValue,
+        highlightedIndex,
+        selectedItem,
+        getRootProps,
+      }) => (
+        <div
+          style={{ width: 320, height: 260 }}
+          className="bg-primary-700 flex flex-col"
+        >
+          {/* <label {...getLabelProps()}>Enter a fruit</label> */}
+          <div
+            style={{ display: "inline-block" }}
+            className="w-full"
+            {...getRootProps({}, { suppressRefError: true })}
+          >
+            <Input {...getInputProps({ autoFocus: true })} />
           </div>
-        )}
-      </Downshift>
-    </div>
+          <ul
+            {...getMenuProps({
+              className: "overflow-auto flex-1",
+            })}
+          >
+            {isOpen
+              ? data?.names
+                  .filter((item) => !inputValue || item.includes(inputValue))
+                  .map((item, index) => (
+                    <li
+                      {...getItemProps({
+                        key: item,
+                        index,
+                        item: { value: item },
+                        style: {
+                          backgroundColor:
+                            highlightedIndex === index ? "lightgray" : "#000",
+                          fontWeight: selectedItem === item ? "bold" : "normal",
+                        },
+                      })}
+                    >
+                      {item}
+                    </li>
+                  ))
+              : null}
+          </ul>
+        </div>
+      )}
+    </Downshift>
   );
 };
