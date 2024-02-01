3711a76738fc7691a5f630253b586f92abdac396
Ben Awad <benawad97@gmail.com>
2024-02-01 16:47:56 -0600

    add index

--- api/src/schema/projects.ts
+++ api/src/schema/projects.ts
@@ -1,15 +1,23 @@
 import { relations, sql } from "drizzle-orm";
-import { pgTable, text, uuid } from "drizzle-orm/pg-core";
+import { index, pgTable, text, uuid } from "drizzle-orm/pg-core";
 import { projectUsers } from "./project-users";
 import { boards } from "./boards";
 
-export const projects = pgTable("projects", {
-  id: uuid("id")
-    .primaryKey()
-    .default(sql`uuid_generate_v4()`),
-  name: text("name").notNull(),
-  apiKey: text("api_key").notNull(),
-});
+export const projects = pgTable(
+  "projects",
+  {
+    id: uuid("id")
+      .primaryKey()
+      .default(sql`uuid_generate_v4()`),
+    name: text("name").notNull(),
+    apiKey: text("api_key").notNull(),
+  },
+  (table) => {
+    return {
+      apiKeyIdx: index("api_key_idx").on(table.name),
+    };
+  }
+);
 
 export const projectRelations = relations(projects, ({ many }) => ({
   projectUsers: many(projectUsers),
