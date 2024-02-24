47096798c470a729806e54ea6d0a4c9d5d05430c
linda <linda@voidpet.com>
2024-02-24 14:34:58 -0600

    soft entrance

--- web/src/app/ui/FloatingMenu.tsx
+++ web/src/app/ui/FloatingMenu.tsx
@@ -13,7 +13,7 @@ export const FloatingMenu: React.FC<
     <div
       className={
         (className ? className : "bg-primary-800 border-primary-700") +
-        " p-1 ml-8 text-left border rounded-lg shadow-xl "
+        " soft-entrance p-1 ml-8 text-left border rounded-lg shadow-xl "
       }
       style={{ width: autoWidth ? undefined : 200, ...style }}
     >
--- web/src/app/ui/FloatingTooltip.tsx
+++ web/src/app/ui/FloatingTooltip.tsx
@@ -6,7 +6,7 @@ export const FloatingTooltip: React.FC<
   React.PropsWithChildren<FloatingTooltipProps>
 > = ({ children }) => {
   return (
-    <div className="bg-primary-100 -mt-10 p-2 rounded-lg shadow-lg text-primary-900 text-sm z-20">
+    <div className="soft-entrance bg-primary-100 -mt-10 p-2 rounded-lg shadow-lg text-primary-900 text-sm z-20">
       {children}
     </div>
   );
