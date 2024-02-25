43d5d0275cdef274b349c78b4f705edc29471589
Ben Awad <benawad97@gmail.com>
2024-02-25 15:42:27 -0600

    Refactor server startup code

--- api/src/index.ts
+++ api/src/index.ts
@@ -12,14 +12,18 @@ const startServer = async () => {
   console.log("about to migrate postgres");
   await migrate(db, { migrationsFolder: path.join(__dirname, "../drizzle") });
   console.log("postgres migration complete");
-  // await runClickhouseMigrations();
-  // await kafkaProducer.connect();
-  // addIngestRoute(app);
-  // addUpdatePeopleRoute(app);
+  console.log("about to migrate clickhouse");
+  await runClickhouseMigrations();
+  console.log("clickhouse migration complete");
+  console.log("about to connect to kafka");
+  await kafkaProducer.connect();
+  console.log("connected to kafka");
+  addIngestRoute(app);
+  addUpdatePeopleRoute(app);
 
-  // app.listen(4001, () => {
-  //   console.log("server started on http://localhost:4001/trpc");
-  // });
+  app.listen(4001, () => {
+    console.log("server started on http://localhost:4001/trpc");
+  });
 };
 
 startServer();
