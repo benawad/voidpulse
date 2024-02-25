ed5580c5f4bd2848bad160e512ce40c003d272e7
Ben Awad <benawad97@gmail.com>
2024-02-25 15:41:25 -0600

    Add console logs and comment out unused code

--- api/src/index.ts
+++ api/src/index.ts
@@ -9,15 +9,17 @@ import { addIngestRoute } from "./routes/express/ingest";
 import { addUpdatePeopleRoute } from "./routes/express/update-people";
 
 const startServer = async () => {
+  console.log("about to migrate postgres");
   await migrate(db, { migrationsFolder: path.join(__dirname, "../drizzle") });
-  await runClickhouseMigrations();
-  await kafkaProducer.connect();
-  addIngestRoute(app);
-  addUpdatePeopleRoute(app);
+  console.log("postgres migration complete");
+  // await runClickhouseMigrations();
+  // await kafkaProducer.connect();
+  // addIngestRoute(app);
+  // addUpdatePeopleRoute(app);
 
-  app.listen(4001, () => {
-    console.log("server started on http://localhost:4001/trpc");
-  });
+  // app.listen(4001, () => {
+  //   console.log("server started on http://localhost:4001/trpc");
+  // });
 };
 
 startServer();
