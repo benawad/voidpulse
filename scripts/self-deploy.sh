ec7ef98953beabcf586f6da7699da06198867f4e
Ben Awad <benawad97@gmail.com>
2024-02-26 09:52:58 -0600

    Update Docker installation method

--- scripts/self-deploy.sh
+++ scripts/self-deploy.sh
@@ -16,11 +16,8 @@ fi
 sudo apt-get update && sudo apt-get upgrade -y
 
 # Install Docker
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-sudo apt-get update
-sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh ./get-docker.sh
 
 apt install docker-compose
 
