aabba3aa9026ecb8511e3d378b528ec49280a46a
Ben Awad <benawad97@gmail.com>
2024-02-26 10:16:03 -0600

    Update Dockerfile and update.sh script

--- update.sh
+++ update.sh
@@ -2,5 +2,5 @@
 set -e
 git pull
 docker build -t web:latest -f ./web/Dockerfile .
-sed -e '/# @inject-env-variables/r'<(sed 's/^/ENV /' .env) -e '/# @inject-env-variables/d' api/Dockerfile > Dockerfile.api.tmp ; docker build -t api:latest -f ./Dockerfile.api.tmp ./api
+sed -e '/# @inject-env-variables/r'<(sed 's/^/ENV /' ../.env) -e '/# @inject-env-variables/d' api/Dockerfile > Dockerfile.api.tmp ; docker build -t api:latest -f ./Dockerfile.api.tmp ./api
 docker-compose up -d
