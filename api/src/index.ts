b7067a945e3921a5560da8534ce3dd9121e7d8be
Ben Awad <benawad97@gmail.com>
2024-02-25 20:00:14 -0600

    Add clickhouse connection check and retry mechanism

--- api/src/index.ts
+++ api/src/index.ts
@@ -2,17 +2,31 @@ import "dotenv-safe/config";
 import { migrate } from "drizzle-orm/node-postgres/migrator";
 import path from "path";
 import { app } from "./appRouter";
-import { runClickhouseMigrations } from "./clickhouse";
+import { clickhouse, runClickhouseMigrations } from "./clickhouse";
 import { db } from "./db";
 import { kafkaProducer } from "./kafka/kafka";
 import { addIngestRoute } from "./routes/express/ingest";
 import { addUpdatePeopleRoute } from "./routes/express/update-people";
 import { __prod__ } from "./constants/prod";
+import { sleep } from "./utils/sleep";
 
 const startServer = async () => {
   console.log("about to migrate postgres");
   await migrate(db, { migrationsFolder: path.join(__dirname, "../drizzle") });
   console.log("postgres migration complete");
+  console.log("see if clickhouse connection is working");
+  for (let i = 0; i++; i < 10) {
+    try {
+      clickhouse.query({ query: "SELECT 1" });
+      break;
+    } catch {}
+    console.log(
+      "clickhouse connection failed, sleeping for " + (i + 1) + " seconds"
+    );
+    sleep((i + 1) * 1000);
+    console.log("retrying now...");
+  }
+  console.log("connected to clickhouse");
   console.log("about to migrate clickhouse");
   await runClickhouseMigrations();
   console.log("clickhouse migration complete");
---
+++ api/src/utils/sleep.ts
@@ -0,0 +1,2 @@
+export const sleep = (ms = 1000) =>
+  new Promise((resolve) => setTimeout(resolve, ms));
